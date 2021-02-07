836b9baef8da6d4c7591da60332617ebed41a68b
CH-KevinDubois <kvn.dubois@gmail.com>
2021-02-07 22:24:13 +0100

    Model Product + Table

---
+++ database/migrations/2021_02_07_212203_create_products_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->date('expired_at');
+            $table->integer('quantity');
+            $table->mediumText('name');
+            $table->mediumText('description');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
