febc86b09d48dc50373e3bf39a70b68cc2eb6448
CH-KevinDubois <kvn.dubois@gmail.com>
2021-03-01 22:01:21 +0100

    Add button description

--- resources/views/fridges/index.blade.php
+++ resources/views/fridges/index.blade.php
@@ -24,38 +24,38 @@
                                     </svg>
                                     <p class="leading-snug text-gray-800 text-xl">{{ $fridge->location }}</p>
                                 </div>
-                                <div class="flex flex-row justify-evenly space-x-3">
+                                <div class="flex flex-col space-y-2">
                                     <!-- add product -->
-                                    <form action="{{ route('products.create', $fridge) }}" method="post" class="flex items-center">
+                                    <form action="{{ route('products.create', $fridge) }}" method="post">
                                         @csrf
                                         @method('get')
-                                        <button type="submit" class="border border-blue-800 shadow-md p-2 flex items-center justify-center bg-blue-800 rounded-lg
-                                        focus:outline-none hover:bg-blue-900">
-                                            <svg class="w-12 h-12 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                        <button type="submit" class="flex items-center space-x-2 btn-blue bg-blue-800 hover:bg-blue-900 w-auto">
+                                            <svg class="w-6 h-6 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
                                             </svg>
+                                            <span>Create product</span>
                                         </button>
                                     </form>
                                     <!-- see products -->
-                                    <form action="{{ route('products.index', $fridge) }}" method="post" class="flex items-center">
+                                    <form action="{{ route('products.index', $fridge) }}" method="post">
                                         @csrf
                                         @method('get')
-                                        <button type="submit" class="border border-blue-500 shadow-md p-2 flex items-center justify-center bg-blue-500 rounded-lg
-                                        focus:outline-none hover:bg-blue-600">
-                                            <svg class="w-12 h-12 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                        <button type="submit" class="flex items-center space-x-2 btn-blue w-auto">
+                                            <svg class="w-6 h-6 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-3 7h3m-3 4h3m-6-4h.01M9 16h.01" />
                                             </svg>
+                                            <span>Products list</span>
                                         </button>
                                     </form>
                                     <!-- show fridge info -->
-                                    <form action="{{ route('fridges.show', $fridge)}}" method="post" class="flex items-center">
+                                    <form action="{{ route('fridges.show', $fridge)}}" method="post">
                                         @csrf
                                         @method('get')
-                                        <button type="submit" class="border border-blue-500 shadow-md p-2 flex items-center justify-center bg-blue-500 rounded-lg
-                                        focus:outline-none hover:bg-blue-600">
-                                            <svg class="w-12 h-12 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                        <button type="submit" class="flex items-center space-x-2 btn-blue w-auto">
+                                            <svg class="w-6 h-6 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 12h.01M12 12h.01M16 12h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
                                             </svg>
+                                            <span>Parameters</span>
                                         </button>
                                     </form>
                                 </div>
