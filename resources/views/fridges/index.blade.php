d50857f4705c952f95989592714df8c6f79cce5b
CH-KevinDubois <kvn.dubois@gmail.com>
2021-03-02 21:10:46 +0100

    Fix buttons

--- resources/views/fridges/index.blade.php
+++ resources/views/fridges/index.blade.php
@@ -9,27 +9,27 @@
                     <div class="py-4 mx-4">
                         <div class="w-72">
                             <!-- gray background -->
-                            <div class="bg-gray-300 text-gray-50 rounded-xl p-8 space-y-5">
+                            <div class="bg-gray-300 text-gray-50 rounded-xl p-7 space-y-2">
                             <!-- blue line -->
                                 <div class="h-2 w-20 bg-blue-500"></div>
-                                <!-- completion -->
-                                <div class="text-xl font-extrabold text-gray-800">{{ ($fridge->count_products()/$fridge->max_capacity)*100 }}%</div>
-                                <!-- description -->
+                                <!-- fridge completion -->
+                                <div class="text-xl font-extrabold text-gray-800">{{ round($fridge->count_products()/$fridge->max_capacity,3)*100 }}%</div>
+                                <!-- brand/description/model -->
                                 <p class="leading-snug text-gray-800 font-bold text-2xl">{{ $fridge->brand }} </p>
-                                <!-- description -->
-                                <div class="flex items-center pb-5 space-x-2">
+                                <!-- location -->
+                                <div class="flex items-center pb-2 space-x-2">
                                     <svg class="w-6 h-6 text-gray-800" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z" />
                                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 11a3 3 0 11-6 0 3 3 0 016 0z" />
                                     </svg>
                                     <p class="leading-snug text-gray-800 text-xl">{{ $fridge->location }}</p>
                                 </div>
-                                <div class="flex flex-col space-y-2">
+                                <div class="flex flex-col w-10/12 space-y-2">
                                     <!-- add product -->
                                     <form action="{{ route('products.create', $fridge) }}" method="post">
                                         @csrf
                                         @method('get')
-                                        <button type="submit" class="flex items-center space-x-2 btn-blue bg-blue-800 hover:bg-blue-900 w-auto">
+                                        <button type="submit" class="flex items-center space-x-2 btn-blue bg-blue-800 hover:bg-blue-900">
                                             <svg class="w-6 h-6 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
                                             </svg>
@@ -40,7 +40,7 @@
                                     <form action="{{ route('products.index', $fridge) }}" method="post">
                                         @csrf
                                         @method('get')
-                                        <button type="submit" class="flex items-center space-x-2 btn-blue w-auto">
+                                        <button type="submit" class="flex items-center space-x-2 btn-blue">
                                             <svg class="w-6 h-6 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-3 7h3m-3 4h3m-6-4h.01M9 16h.01" />
                                             </svg>
@@ -51,7 +51,7 @@
                                     <form action="{{ route('fridges.show', $fridge)}}" method="post">
                                         @csrf
                                         @method('get')
-                                        <button type="submit" class="flex items-center space-x-2 btn-blue w-auto">
+                                        <button type="submit" class="flex items-center space-x-2 btn-blue">
                                             <svg class="w-6 h-6 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 12h.01M12 12h.01M16 12h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
                                             </svg>
